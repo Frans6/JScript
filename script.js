f77ce11c4694116c9a128ead476028ea9e421969
Frans6 <jefferson.frds@gmail.com>
2022-11-19 08:33:43 -0300

    add

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 /*
- capture 10 itens para compor a lista de um supermercado
+ apresente a mensagem ao usuário:
+ "adivinhe o número que estou pensando? Está entre
+ 0 e 10"
 
- após capturar os 10 itens, imprima-os separando por vírgula
+ crie uma lógica para gerar um número aleatório e 
+ verificar se o número digitado é o mesmo que o 
+ aleatório gerado pelo sistema.
+
+ enquanto o usuário não adivinhar o número, mostrar
+ a mensagem: Erro, tente novamente:
+
+ caso o usuário acerte o número, apresentar a mensagem
+ Parabéns! você adivinhou o número em X tentativas
+
+ substitua o x da mensagem, pelo número de tentativas
+
 */
 
-let iten = [] 
+let result = prompt("adivinhe o número que estou pensando? Está entre 0 e 10")
+const randomNumber = Math.round(Math.random() * 10)
 
-for (let i = 0; i < 10; i++) {
-  iten[i] = prompt(`${i + 1} item: `)
-  
+let i = 1
+
+while( Number(result) != randomNumber ){
+  result = prompt("Erro, tente novamente: ")
+  i ++
 }
 
-alert(iten)
+alert(`Parabéns! Você adivinhou o número em ${i} tentativas`)
